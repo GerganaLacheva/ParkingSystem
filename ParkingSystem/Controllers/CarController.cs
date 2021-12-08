cee51b631aa801553e4dbd6ad8a88d8a1fbc3f9e
User <User@ACCOUNTINGBG1>
2021-12-08 09:15:21 +0200

    Add project files.

---
+++ ParkingSystem/Controllers/CarController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParkingSystem.Data.Models;
+using System.Linq;
+
+namespace ParkingSystem.Controllers
+{
+    public class CarController : Controller
+    {
+        [HttpPost]
+        public IActionResult AddCar(Car car)
+        {
+            DataAccess.Cars.Add(car);
+            return Redirect("/");   //redirect to homepage
+        }
+
+        [HttpPost]
+        public IActionResult DeleteCar(string plateNumber)
+        {
+            Car car = DataAccess.Cars.FirstOrDefault(x => x.PlateNumber == plateNumber);    //Default value=null;
+            DataAccess.Cars.Remove(car);
+            return Redirect("/");
+        }
+    }
+}
---
+++ ParkingSystem/Data/Models/Car.cs
@@ -0,0 +1,10 @@
+﻿namespace ParkingSystem.Data.Models
+{
+    public class Car
+    {
+        public string CarMake { get; set; }
+        public string PlateNumber { get; set; }
+        public string ReleaseDate { get; set; }
+        public string Price { get; set; }
+    }
+}
